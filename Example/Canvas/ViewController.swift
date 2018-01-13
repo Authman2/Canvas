6d937a16de638f922e01a9999b21e6a5bd7b424b
Adeola Uthman <authman2@gmail.com>
2018-01-12 22:05:52 -0500

    Initial commit

---
+++ Example/Canvas/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Canvas
+//
+//  Created by authman2 on 01/12/2018.
+//  Copyright (c) 2018 authman2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
