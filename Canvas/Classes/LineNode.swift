ea2e7542a656d1e2d0b3530f157eeb6b0596e6e8
Adeola Uthman <authman2@gmail.com>
2018-03-11 23:48:27 -0400

    Basics of moving straight lines

--- Canvas/Classes/LineNode.swift
+++ Canvas/Classes/LineNode.swift
@@ -50,6 +50,30 @@ class LineNode: Node {
         return points.contains(where: { (p) -> Bool in return abs(point.x - p.x) <= 5 && abs(point.y - p.y) <= 5 })
     }
     
+    override func moveNode(to: CGPoint) {
+        let dx = lastPoint.x - firstPoint.x
+        let dy = lastPoint.y - firstPoint.y
+        var dists: [(CGFloat, CGFloat)] = []
+        
+        for i in 0..<points.count {
+            let dx = points[i].x - firstPoint.x
+            let dy = points[i].y - firstPoint.y
+            
+            dists.append((dx,dy))
+        }
+        
+        firstPoint.x = firstPoint.x - (firstPoint.x - to.x)
+        firstPoint.y = firstPoint.y - (firstPoint.y - to.y)
+        
+        lastPoint.x = firstPoint.x + dx
+        lastPoint.y = firstPoint.y + dy
+        
+        for i in 0..<points.count {
+            points[i].x = firstPoint.x + dists[i].0
+            points[i].y = firstPoint.y + dists[i].1
+        }
+    }
+    
     override func draw() {
         guard let context = UIGraphicsGetCurrentContext() else { return }
         
